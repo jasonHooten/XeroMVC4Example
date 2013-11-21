2305b43206f068945864a8fff5b1b68332e1eff8
Jason Hooten <jason.hooten@gmail.com>
2013-11-21 13:49:07 -0600

    added a playground for building reports

--- XeroMVC4Example/Controllers/XeroController.cs
+++ XeroMVC4Example/Controllers/XeroController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.Web.Mvc;
+using XeroApi.Model.Reporting;
 using XeroMVC4Example.Filters;
 using XeroMVC4Example.Services;
 
@@ -16,6 +18,7 @@ namespace XeroMVC4Example.Controllers
 
 			if (oauthSession.HasValidAccessToken && repository != null && repository.Organisation != null)
 			{
+				Playroom();
 				return new RedirectResult("~/");
 			}
 
@@ -42,10 +45,49 @@ namespace XeroMVC4Example.Controllers
 			var verificationCode = Request.Params["oauth_verifier"];
 
 			var accessToken = oauthSession.ExchangeRequestTokenForAccessToken(verificationCode);
-			var xeroRepo = XeroProvider.GetCurrentRepository();
-			var xeroOrganisation = xeroRepo.Organisation;
-
+			Playroom();
+			
 			return Redirect("~/");
+		}
+
+		private static void Playroom()
+		{
+			var xeroRepo = XeroProvider.GetCurrentRepository();
+			if (xeroRepo.Organisation != null)
+			{
+				var panlReport = xeroRepo.Reports.RunDynamicReport(new ProfitAndLossReport());
+				var balanceReport = xeroRepo.Reports.RunDynamicReport(new BalanceSheetReport());
+				var arReport = xeroRepo.Reports.RunDynamicReport(new AgedPayablesByContactReport());
+				var apReport = xeroRepo.Reports.RunDynamicReport(new AgedPayablesByContactReport());
+
+				if (panlReport != null)
+				{
+					foreach (var reportTitle in panlReport.ReportTitles)
+					{
+						Console.WriteLine("\t" + reportTitle);
+					}
+
+					foreach (var reportRow in panlReport.Rows)
+					{
+						Console.WriteLine(" " + reportRow.Title);
+
+						if (reportRow.Rows != null)
+						{
+							foreach (var subRow in reportRow.Rows)
+							{
+								Console.Write(" Row: " + subRow.RowType);
+
+								foreach (var cell in subRow.Cells)
+								{
+									Console.Write(cell.Value + ", ");
+								}
+
+								Console.WriteLine();
+							}
+						}
+					}
+				}
+			}
 		}
     }
 }
